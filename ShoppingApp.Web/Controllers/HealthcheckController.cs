67f418e80bd482b319830a6dd5e06af620b4942c
Sly <simplysly23@gmail.com>
2026-07-27 20:56:33 +0200

    Added healthcheck

---
+++ ShoppingApp.Web/Controllers/HealthcheckController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Core.Utility;
+
+namespace ShoppingApp.Web.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class HealthcheckController : ControllerBase
+{
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Get()
+    {
+        return Ok(Result.Success());
+    }
+}
